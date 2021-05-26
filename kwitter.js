73ed1e615571759e54c6b4de48162054cd3cf92f
Weee-boop <79670593+Weee-boop@users.noreply.github.com>
2021-05-26 12:23:38 -0500

    Add files via upload

--- kwitter.js
+++ kwitter.js
@@ -1,7 +1,22 @@
-function addUser() {
-    user_name=document.getElementById("user_name").value;
-
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDEZ9_bqRWRVoYZYOZS7R9AjGn1hdnnD9M",
+  authDomain: "france-paris-cheese.firebaseapp.com",
+  databaseURL: "https://france-paris-cheese-default-rtdb.firebaseio.com",
+  projectId: "france-paris-cheese",
+  storageBucket: "france-paris-cheese.appspot.com",
+  messagingSenderId: "771011888768",
+  appId: "1:771011888768:web:44c0ba6c2d1960e6a6c865"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  function addUser() {
+  
+    user_name = document.getElementById("user_name").value;
+  
     localStorage.setItem("user_name", user_name);
-
-    window.location="kwitter_room.html"
-} 
+    
+      window.location = "kwitter_room.html";
+  }
+  
